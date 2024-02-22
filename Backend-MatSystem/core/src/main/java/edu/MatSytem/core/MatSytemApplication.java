3dac4afae9eed64ef229aea5c859db23a686e241
Yanuard Bonilla <113991827+Y-S-500@users.noreply.github.com>
2024-02-21 20:20:55 -0500

    Backend-proyecto

---
+++ Backend-MatSystem/core/src/main/java/edu/MatSytem/core/MatSytemApplication.java
@@ -0,0 +1,13 @@
+package edu.MatSytem.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatSytemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MatSytemApplication.class, args);
+	}
+
+}
---
+++ Backend-MatSystem/core/src/test/java/edu/MatSytem/core/MatSytemApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.MatSytem.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatSytemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
